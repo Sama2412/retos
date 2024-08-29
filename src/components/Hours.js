cabf4de72c877616f9ee4eda9481f6a048121977
Sama2412 <142283982+Sama2412@users.noreply.github.com>
2024-08-29 21:32:26 +0000

    reto 4

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,16 @@
 // Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+    const currentDate = new Date();
+    const hours = currentDate.getHours();
+
+    return (
+        <div>
+            <h2>Current Hour</h2>
+            <p>The current hour is: {hours}</p>
+        </div>
+    );
+};
+
+export default Hours;
